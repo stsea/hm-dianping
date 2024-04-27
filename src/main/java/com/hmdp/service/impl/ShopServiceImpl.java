12b99255db5dd005848ca57f8f78ccd55392f0f0
wsl <2912167574@qq.com>
2024-04-27 22:19:58 +0800

    mutex解决缓存击穿

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
@@ -8,6 +9,7 @@ import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisConstants;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -40,6 +42,23 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
      */
     @Override
     public Result queryById(Long id) {
+        //缓存穿透代码
+//        Shop shop = queryWithPassThrough(id);
+
+        //互斥解决缓存击穿
+        Shop shop = queryWithMutex(id);
+        if (shop==null){
+            return Result.fail("店铺不存在");
+        }
+        return Result.ok(shop);
+    }
+
+    /**
+     * 缓存击穿
+     * @param id
+     * @return
+     */
+    public Shop queryWithMutex(Long id){
         String key = CACHE_SHOP_KEY + id;
         //redis 取shop
         String shopJson = stringRedisTemplate.opsForValue().get(key);
@@ -47,21 +66,77 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         if (StrUtil.isNotBlank(shopJson)){
             //存在，直接返回
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return Result.ok(shop);
+            return shop;
         }
         if(shopJson != null){
-            //缓存击穿 ，null直接返回失败
-            return Result.fail("店铺不存在");
+            //缓存穿透 ，null直接返回失败
+            return null;
+        }
+
+        //互斥锁
+        String lockKey = RedisConstants.LOCK_SHOP_KEY + id;
+        Shop shop = null;
+        try {
+            boolean isLock = tryLock(lockKey);
+            if (!isLock){
+                Thread.sleep(50);
+                queryWithMutex(id);
+            }
+
+            shop = getById(id);
+            if (shop == null){
+                //缓存穿透 ，缓存null
+                stringRedisTemplate.opsForValue().set(key,"",CACHE_SHOP_TTL,TimeUnit.MINUTES);
+                return null;
+            }
+            //存在，写入redis
+            stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //释放lock
+            unLock(lockKey);
+        }
+
+        return shop;
+    }
+    /**
+     * 缓存穿透代码
+     * @param id
+     * @return
+     */
+    public Shop queryWithPassThrough(Long id){
+        String key = CACHE_SHOP_KEY + id;
+        //redis 取shop
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+
+        if (StrUtil.isNotBlank(shopJson)){
+            //存在，直接返回
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return shop;
+        }
+        if(shopJson != null){
+            //缓存穿透 ，null直接返回失败
+            return null;
         }
         Shop shop = getById(id);
         if (shop == null){
-            //缓存击穿 ，缓存null
+            //缓存穿透 ，缓存null
             stringRedisTemplate.opsForValue().set(key,"",CACHE_SHOP_TTL,TimeUnit.MINUTES);
-            return Result.fail("店铺不存在！");
+            return null;
         }
         stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
 
-        return Result.ok(shop);
+        return shop;
+    }
+
+    public boolean tryLock(String key){
+        Boolean b = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(b);
+    }
+    public void unLock(String key){
+        stringRedisTemplate.delete(key);
     }
 
     /**
